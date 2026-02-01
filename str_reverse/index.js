9f37d56218f31e289889c6f87b1d25639b51df2e
akingo55 <43959158+akingo55@users.noreply.github.com>
2026-02-01 16:54:16 +0900

    add func reverse str

---
+++ str_reverse/index.js
@@ -0,0 +1,8 @@
+"use strict";
+function reverseString(str) {
+    return [...str].reverse().join('');
+}
+// ES2015以降の環境で動作するコード
+// tsc -pコマンドでtsconfig.jsonを指定してコンパイル
+console.log(reverseString('Hello'));
+console.log(reverseString('Hello😃'));
